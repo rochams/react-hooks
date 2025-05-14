ea56d02b04667b583605a7d00bb8e67de233ca1b
rochams <msrocha.ad@gmail.com>
2025-05-13 21:55:50 -0300

    feat: adiciona uso de useRef

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect, useRef, useState } from 'react'
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
 
 const UseRef = (props) => {
+
+    const [num, setNum] = useState(0)
+    /**
+     * O seu grande trunfo é não alterar o 'estado' da aplicação, o que permite fazer muitas coisas com esse recurso
+     * sem re-renderizar a página
+     * O useRef também é usado para "controlar/contabilizar" todas as renderizações do componente
+     * Isso é feito alterando o valor do atributo 'current'
+     * Se, por exemplo, eu deseje acompanhar as renderizações somente de um evento específico, posso usar juntamente
+     * com o useEffect por exemplo.
+     * Como no caso abaixo, que adicionamos +1 ao current toda vez que num é alterado
+     */
+    const counter = useRef(0);
+
+    useEffect(function() {
+        counter.current+=1
+    }, [num])
+    
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+            <SectionTitle title="Casos de useRef" />
+            <div>
+                <input type="number" className="input" value={ num } onChange={ e => setNum(e.target.value) }/>
+            </div>
+            <div>
+                <span className="text">[ { counter.current } ]</span>
+            </div>
         </div>
     )
 }
